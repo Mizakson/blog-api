041b158afe103310c88ad722797a4bfec3b17b9d
Mizakson <mizakson.dev@gmail.com>
2025-03-23 15:18:11 -0400

    Add bodyParser statements

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,16 @@
 require("dotenv").config()
 const express = require("express")
-
+const bodyParser = require("body-parser")
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const indexRouter = require("../back-end/routes/indexRouter")
 
 app.use("/", indexRouter)
+// app.use("/sign-up", signUpRouter)
+// app.use("login", loginRouter)
 
 const PORT = 3000
 app.listen(PORT, "0.0.0.0", () => {
