bc7798c873b4cbc4eb91171d91719b2c3aea47b9
Mizakson <mizakson.dev@gmail.com>
2025-03-23 16:29:28 -0400

    Add two methods to usersController.js

---
+++ back-end/controllers/usersController.js
@@ -0,0 +1,13 @@
+const queries = require("../utils/queries")
+
+exports.signUpGet = async function (req, res) {
+    res.status(200).json({
+        message: "Hi there from sign-up get",
+    })
+}
+
+exports.signUpPost = async function (req, res) {
+    res.status(200).json({
+        message: "Hi there from sign-up post",
+    })
+}
