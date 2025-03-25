078229ea294dfe5d7d672fe38065ea0114fc0955
Mizakson <mizakson.dev@gmail.com>
2025-03-25 16:56:58 -0400

    Get basic implementation of role-enforced route protection

--- back-end/routes/posts.js
+++ back-end/routes/posts.js
@@ -5,16 +5,35 @@ const jwt = require("jsonwebtoken")
 const middleware = require("../middleware/middleware")
 
 postsRouter.post("/", middleware.verifyToken, (req, res) => {
-    jwt.verify(req.token, process.env.JWT_SECRET_KEY, (err, data) => {
-        if (err) {
-            res.sendStatus(403)
-        } else {
-            res.status(200).json({
-                message: "You made it to the protected route :)",
-                data,
-            })
-        }
-    })
+
+
+    if (req.role === "AUTHOR") {
+        jwt.verify(req.token, process.env.JWT_SECRET_KEY, (err, data) => {
+            if (err) {
+                res.json({ error: err.message })
+            } else {
+                res.status(200).json({
+                    message: "You made it to the protected route :)",
+                    data,
+                })
+            }
+        })
+    }
+
+    if (req.role !== "AUTHOR") {
+        res.status(403).json({ message: "You have a BASIC role, not an author role!" })
+    }
+
+    // jwt.verify(req.token, process.env.JWT_SECRET_KEY, (err, data) => {
+    //     if (err) {
+    //         res.json({ error: err.message })
+    //     } else {
+    //         res.status(200).json({
+    //             message: "You made it to the protected route :)",
+    //             data,
+    //         })
+    //     }
+    // })
 
 })
 
